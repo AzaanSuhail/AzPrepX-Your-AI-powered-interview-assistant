13551930566a325f154e39bd684b1ed50c7d5a2d
Azaan Suhail <azaan.suhail@gmail.com>
2025-04-28 16:53:37 +0530

    Home page : Almost completed, interview wise cards functionality added with relevant logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,39 @@
-import {ReactNode} from 'react'
+// import {ReactNode} from 'react'
 
-const RootLayout=(
-    { children }: { children: ReactNode 
-}) => { 
+// const RootLayout=(
+//     { children }: { children: ReactNode 
+// }) => { 
+//     return (
+//     <div>{children}</div>
+// )
+// }
+
+
+// export default RootLayout
+
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+
+//? import { isAuthenticated } from "@/lib/actions/auth.action";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+    // const isUserAuthenticated = await isAuthenticated();
+    // if (!isUserAuthenticated) redirect("/sign-in");
+
     return (
-    <div>{children}</div>
-)
-}
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex items-center gap-2">
+                    <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+                    <h2 className="text-primary-100">Interview - Genie</h2>
+                </Link>
+            </nav>
 
+            {children}
+        </div>
+    );
+};
 
-export default RootLayout
+export default Layout;
