bf6c0e772302dfb79d86c1d712d4d2ae68e39cba
Azaan Suhail <azaan.suhail@gmail.com>
2025-05-02 12:56:02 +0530

    VAPI interview generation partially completed

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,4 +1,5 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
 const page = () => {
     return (
@@ -6,8 +7,9 @@ const page = () => {
             <h3>Interview generation</h3>
 
             <Agent
-                userName="you"
-                userId="user1"
+                userName={user?.name!}
+                userId={user?.id}
+                profileImage={user?.profileURL}
                 type="generate"
             />
         </>
